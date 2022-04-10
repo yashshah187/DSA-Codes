cb3164aec80fb2d2f70fcf09135c80a032358f24
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-04-10 13:37:08 +0530

    Time: 4 ms (54.24%), Space: 42 MB (68.25%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calPoints(String[] ops) {
+        List<Integer> list=new ArrayList<>();
+        for(String s:ops)
+        {
+            switch(s)
+            {
+                case "C":int size=list.size()-1;
+                         list.remove(size);
+                         break;
+                case "D":list.add(list.get(list.size()-1)*2);
+                         break;
+                case "+":list.add(list.get(list.size()-1)+list.get(list.size()-2));
+                         break;
+                default: list.add(Integer.parseInt(s));
+                         break;
+            }
+           
+            
+            
+            
+            }
+    
+    int ans=0;
+    for(int x:list)
+    {
+        ans+=x;
+    }
+        
+        return ans;
+    }
+}
