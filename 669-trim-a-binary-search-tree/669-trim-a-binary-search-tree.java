3de23cc5472b845e111953d30a2249e19f1f524e
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-04-15 09:50:56 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (84.81%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -19,13 +19,13 @@ class Solution {
         
         if(root.val>high)
         {
-            return trimBST(root.left,low,high);
+            return trimBST(root.left,low,high); // As it is a bst if val of parent is greater tha high all other on right side will greater so triming it
         }
         if(root.val<low)
         {
-            return trimBST(root.right,low,high);
+            return trimBST(root.right,low,high);  // As it is a bst if val of parent is less tha low all other on left side will lesser so triming it
         }
-        
+     
         root.left=trimBST(root.left,low,high);
         root.right=trimBST(root.right,low,high);
         return root;
