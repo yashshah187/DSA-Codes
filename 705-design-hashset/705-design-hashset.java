1b699badf7f78e8f47f646e872dc208b632375ed
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-04-21 09:31:13 +0530

    Time: 44 ms (35.78%), Space: 84.2 MB (13.77%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,19 +1,19 @@
 class MyHashSet {
-HashSet<Integer> set;
+boolean [] a;
     public MyHashSet() {
-        set=new HashSet<>();
+        a=new boolean[1000001];
     }
     
     public void add(int key) {
-        set.add(key);
+        a[key]=true;
     }
     
     public void remove(int key) {
-        set.remove(key);
+        a[key]=false;
     }
     
     public boolean contains(int key) {
-        return set.contains(key);
+        return a[key];
     }
 }
 
