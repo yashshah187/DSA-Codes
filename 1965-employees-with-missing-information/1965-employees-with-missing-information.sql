93044a5b55f4e0d4661efab5e71719f89f25675d
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-07-21 10:48:04 +0530

    Time: 939 ms (21.85%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 
 
-select e.employee_id from Employees e where e.employee_id not in(select a.employee_id from Employees a, Salaries b where a.employee_Id=b.employee_Id) union select s.employee_Id from Salaries s where s.employee_id not in(select a.employee_id from Employees a, Salaries b where a.employee_Id=b.employee_Id) order by employee_Id;
+# select e.employee_id from Employees e where e.employee_id not in(select a.employee_id from Employees a, Salaries b where a.employee_Id=b.employee_Id) union select s.employee_Id from Salaries s where s.employee_id not in(select a.employee_id from Employees a, Salaries b where a.employee_Id=b.employee_Id) order by employee_Id;
+
+select employee_Id from Employees where employee_Id not in (select employee_Id from Salaries)
+union
+select employee_Id from Salaries where employee_Id not in (select employee_Id from Employees) order by 1 ASC;
