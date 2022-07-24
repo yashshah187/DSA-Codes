48ef757b73770b8b8b50eddae1e24bf70dd4a618
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-07-24 23:18:08 +0530

    Time: 1678 ms (5.01%), Space: 54.7 MB (11.42%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,31 +1,37 @@
 class Solution {
-    public void fun(int[] nums,boolean[] freq, List<List<Integer>> ans,List<Integer> ds)
+    public void fun(int[] nums,List<List<Integer>> ans,int index)
     {
-        if(ds.size()==nums.length)
+        if(index==nums.length)
         {
-            if(!ans.contains(ds))
-            ans.add(new ArrayList<>(ds));
+            List<Integer> t=new ArrayList<>();
+            for(int x:nums)
+            {
+                t.add(x);
+            }
+            
+            if(!ans.contains(t))
+            ans.add(new ArrayList<>(t));
             
             return;
         }
         
-        for(int i=0;i<nums.length;i++)
+        for(int i=index;i<nums.length;i++)
         {
-            if(freq[i]==false)
-            {
-                ds.add(nums[i]);
-                freq[i]=true;
-                fun(nums,freq,ans,ds);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+                swap(i,index,nums);
+                fun(nums,ans,index+1);
+                swap(i,index,nums);
         }
+    }
+    public void swap(int i,int index,int[] nums)
+    {
+        int temp=nums[i];
+        nums[i]=nums[index];
+        nums[index]=temp;
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
 
-        Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
-        fun(nums,new boolean[nums.length],ans,new ArrayList<>());
+        fun(nums,ans,0);
         return ans;
        
     }
