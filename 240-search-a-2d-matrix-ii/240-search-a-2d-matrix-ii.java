23074a264798f2c3ac8b0903d8402cb9b5f05256
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-07-24 07:59:00 +0530

    Time: 20 ms (9.79%), Space: 58.1 MB (25.76%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -2,17 +2,15 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rl=matrix.length-1;
         int cl=matrix[0].length-1;
+
+        int row=0,column=0;
         
-        int column=0;
         
-        for(int row=0;row<matrix.length;row++)
+        while(row<=rl && column<=cl )
         {
             if(matrix[row][0]==target || matrix[row][cl]==target) return true;
-            
-            if(column<cl)
-            {
-                 if(matrix[0][column]==target || matrix[rl][column]==target) return true;
-            }
+            if(matrix[0][column]==target || matrix[rl][column]==target) return true;
+        
            
             
             if(matrix[row][0]<target && matrix[row][cl]>target)
@@ -23,7 +21,7 @@ class Solution {
                 }
             }
             
-            if(column < cl && matrix[0][column]<target && matrix[rl][column]>target)
+            if(matrix[0][column]<target && matrix[rl][column]>target)
             {
                  for(int i=1;i<matrix.length-1;i++)
                 {
@@ -31,6 +29,7 @@ class Solution {
                 }
             }
             
+            row++;
             column++;
         }
         
