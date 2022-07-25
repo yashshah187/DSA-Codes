e49f644583e4112fa96fe3dbd27b59a117d79b90
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-07-25 14:05:06 +0530

    Time: 24 ms (34.25%), Space: 41.8 MB (39.25%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,54 @@
+class Solution {
+    
+    public  void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board)
+    {
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board.length;j++)
+            {
+
+                if(board[i][j]=='.')
+                {
+                     for(char c='1';c<='9';c++)
+                      {
+ 
+                        if(isValid(c,i,j,board))
+                        {
+                            board[i][j]=c;
+
+                            if(solve(board))
+                            return true;
+                            else
+                            board[i][j]='.';
+                            
+                        }
+                    }
+                    
+                    return false;
+                }
+                
+            }
+        }
+        
+    return true;
+    }
+
+   public boolean isValid(char c,int row,int col,char board[][])
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false; 
+        }
+        
+        return true;
+    }
+    
+    
+}
