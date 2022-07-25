4ce29df9b0d8e92ca80fbfbef2b87b290ef95c2d
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-07-25 10:08:22 +0530

    Time: 1157 ms (15.50%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select user_id, max(time_stamp) as last_stamp from Logins where time_stamp<="2020-12-31 23:59:59" and time_stamp>="2020-01-01 00:00:00" group by user_id;
+
+select user_id, max(time_stamp) as last_stamp from Logins where time_stamp like "2020%" group by user_id;
