dcd51021d73c220ea4d1b1de1e303900677538aa
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-08-01 21:35:38 +0530

    Time: 1976 ms (13.13%), Space: 42.2 MB (75.63%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int fun(String s,int index,int[] dp)
-    {
-        if(index>=s.length())
-            return 0;
+    // Memoization
+//     public int fun(String s,int index,int[] dp)
+//     {
+//         if(index>=s.length())
+//             return 0;
         
-        if(dp[index]!=0) return dp[index];
+//         if(dp[index]!=0) return dp[index];
         
-        int ans=Integer.MAX_VALUE;
+//         int ans=Integer.MAX_VALUE;
     
-        for(int i=index;i<s.length();i++)
-        {
+//         for(int i=index;i<s.length();i++)
+//         {
            
-            if(isP(s,index,i))
-            {
-                int cost=1+fun(s,i+1,dp);
-                ans=Math.min(ans,cost);
-            }
+//             if(isP(s,index,i))
+//             {
+//                 int cost=1+fun(s,i+1,dp);
+//                 ans=Math.min(ans,cost);
+//             }
             
-        }
+//         }
         
-        return dp[index]=ans;
+//         return dp[index]=ans;
         
-    }
+//     }
+    
     public boolean isP(String s,int start,int end)
     {
         
@@ -35,11 +37,36 @@ class Solution {
         
         return true;
     }
+    
+    // Tabulation
     public int minCut(String s) {
-        if(s.length()==1) return 0;
-        int[] dp=new int[s.length()];
         
-        return fun(s,0,dp)-1;
+        if(s.length()==1) return 0;
+        
+        int[] dp=new int[s.length()+1];
+        
+        dp[s.length()]=0;
+        
+       
+    for(int i=s.length()-1;i>=0;i--)
+    {
+        int ans=Integer.MAX_VALUE;
+        
+        for(int j=i;j<s.length();j++)
+        {
+            
+            if(isP(s,i,j))
+            {
+                int cost=1+dp[j+1];
+                ans=Math.min(ans,cost);
+            }
+            
+        }
+        
+        dp[i]=ans;
+    }
+        
+        return dp[0]-1;
         
     }
 }
