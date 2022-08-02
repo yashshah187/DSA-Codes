9283f58e732fcfc00c40abde5176c8429b006afe
Yash Shah <79187387+yashshah187@users.noreply.github.com>
2022-08-02 19:18:35 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (43.32%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,21 +1,28 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-                
-        if(nums.length==1)
-        {
-            if( nums[0]>=target)
-                return 0;
-            else
-                return 1;
-        }
+    public int binary(int[] nums,int left,int right,int target)
+    {
+        if(nums[left]>target) return left;
+        if(nums[right]<target) return right+1;
         
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==target || nums[i]>target)
-                return i;
-        }
+        int mid=left+(right-left)/2;
         
-        return nums.length;
+        if(left<right && nums[mid]<target)
+        {
+            return binary(nums,mid+1,right,target);
+        }
+        else if(left<right && nums[mid]>target)
+        {
+            return binary(nums,left,mid-1,target);
+        }
+        else
+        {
+            return mid;
+        }
         
     }
+    public int searchInsert(int[] nums, int target) {
+        return binary(nums,0,nums.length-1,target); 
+    }
 }
+
+// 1 3 4 6
